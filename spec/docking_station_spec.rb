0c1e86a1285e9b4cb0dcb48a375c85f2082440df
Brandon Graves <bgraves14@gmail.com>
2018-07-02 15:49:42 +0100

    added and DRY'd up tests for init taking an argument

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -53,5 +53,16 @@ describe DockingStation do
     it "has a default capacity" do
       expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
+
+    describe 'initialization'  do
+      subject { DockingStation.new }
+      let(:bike) { Bike.new }
+        it 'defaults capacity' do
+          described_class::DEFAULT_CAPACITY.times do
+            subject.dock(bike)
+          end
+          expect { subject.dock(bike) }.to raise_error 'Docking Station Full'
+        end
+    end
   end
 end
